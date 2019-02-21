3a48bd99b414a38411f02d5d6b17efac6ab98494
jess2d <jessicap_silveira@hotmail.com>
2019-02-21 10:24:00 -0300

    Spec:  Url are defined

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -30,13 +30,27 @@ $(function() {
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
+         *  it('URL are defined', function() {
+            var i;
+            for(i= 0; i<allFeeds.length; i++){
+                expect(allFeeds[].url).not.toBe(0);
+            }   
+        });
          */
 
-
+        it('URL are defined', function() {
+            var i;
+            for(i= 0; i<allFeeds.length; i++){
+                expect(allFeeds[i].url).toBeDefined();
+                expect(allFeeds[i].url).not.toBe('');
+                expect(allFeeds[i].url).not.toBe(null);
+            }   
+        });
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+
     });
 
 
